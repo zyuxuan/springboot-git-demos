7ea54c534743e8f9458105d6c8c1c141c9a57503
zyuxuan <357442101@qq.com>
2018-08-04 21:46:05 +0800

    init project

---
+++ src/main/java/com/zyuxuan/springbootgitdemos/SpringbootGitDemosApplication.java
@@ -0,0 +1,12 @@
+package com.zyuxuan.springbootgitdemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitDemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitDemosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zyuxuan/springbootgitdemos/SpringbootGitDemosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zyuxuan.springbootgitdemos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootGitDemosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
